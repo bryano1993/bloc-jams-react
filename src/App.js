44f46c6975b059cb1eebcc71f760ffdc7aae2286
Bryan O'Connor <bryano1993@gmail.com>
2018-04-04 19:49:54 -0700

    assignment-3-Routing:Complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
@@ -18,7 +19,8 @@ class App extends Component {
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
-        </main>
+          <Route path="/album" component={Album} />
+        </main> 
       </div>
     );
   }
